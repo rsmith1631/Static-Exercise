840dfa97d3e6c927a2d664afaf8e78049295cc0c
rsmith1631 <rsmith1631@gmail.com>
2023-07-19 12:52:07 -0700

    Finished TempConverter

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace StaticExercise
+﻿using System.Runtime.CompilerServices;
+
+namespace StaticExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           var celsius = TempConverter.FahrenheitToCelsius(68); 
+
+            Console.WriteLine($"It's {celsius} degrees Celsius today!");
+
+            var farhrenheit = TempConverter.CelsiusToFahrenheit(20);
+            Console.WriteLine($"It's {farhrenheit} degrees today!");
+
+
         }
     }
 }
